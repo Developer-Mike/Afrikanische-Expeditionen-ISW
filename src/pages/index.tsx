fd273c72ed445528ba1458a59c035381b1cbd5cb
Mika Schlup <mika.schlup@gmail.com>
2023-02-01 10:09:16 +0100

    Added rickroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Index({ groups }: {
   return (
     <>
       <Head>
-        <title>Magellan</title>
+        <title>Afrikas Expeditionen</title>
         <meta name="viewport" content= "width=device-width, initial-scale=1.0, user-scalable=no"/>
       </Head>
 
