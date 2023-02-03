2307a4002d841c3ba52ece075095436d9125dd97
Mike <52970344+Developer-Mike@users.noreply.github.com>
2023-02-03 11:06:03 +0100

    No rickroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,6 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
-      if (Math.random() > 0.85) {
-        window.location.href = "https://shattereddisk.github.io/rickroll/rickroll.mp4"
-      }
-    }
-  }, [])
-  
   return <Component {...pageProps} />
 }
